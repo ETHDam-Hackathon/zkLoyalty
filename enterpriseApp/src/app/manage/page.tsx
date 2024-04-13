95a21f0a9e825f18f18c936689d9b305e6131129
flo <florian.lueffe@mway.io>
2024-04-13 17:00:34 +0200

    dyn import scanner

--- enterpriseApp/src/app/manage/page.tsx
+++ enterpriseApp/src/app/manage/page.tsx
@@ -1,15 +1,28 @@
 'use client'
 import { useSignMessage } from 'wagmi'
 import { Identity } from '@semaphore-protocol/identity'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Manage() {
   const { signMessage, data: signature } = useSignMessage()
+  const [commitment, setCommitment] = useState<bigint>()
+  const comm = localStorage.getItem('commitment')
 
   function createIdentity() {
-    const identity = new Identity(signature)
-    console.log(identity)
+    if (comm !== null) {
+      setCommitment(BigInt(comm))
+    } else {
+      const identity = new Identity(signature)
+      setCommitment(identity.commitment)
+      localStorage.setItem('commitment', identity.commitment.toString())
+    }
   }
+
+  useEffect(() => {
+    if (comm !== null) {
+      setCommitment(BigInt(comm))
+    }
+  }, [comm])
 
   useEffect(() => {
     if (signature) {
@@ -21,11 +34,22 @@ export default function Manage() {
     <>
       <h2 className='text-2xl mb-2'>Settings</h2>
 
-      {/* Examples are only used for demo purposes. Feel free to delete this section */}
       <div className='mt-4'>
         <h3 className='text-lg mb-1'>Create Semaphore Enterprise Identity</h3>
-        <button onClick={() => signMessage({ message: 'I am an enterprise user for zkLoyalty.' })}>Sign message</button>
-        ƒ<h3 className='text-lg mb-1 mt-6'>Create Group</h3>
+        {!commitment && (
+          <button
+            className='btn btn-primary'
+            onClick={() => signMessage({ message: 'I am an enterprise user for zkLoyalty.' })}>
+            Create Identity
+          </button>
+        )}
+        {commitment && (
+          <p>
+            Identity created with commitment:{' '}
+            <code>{commitment.toString().slice(0, 3) + '...' + commitment.toString().slice(-3)}</code>
+          </p>
+        )}
+        <h3 className='text-lg mb-1 mt-6'>Create Group</h3>
         <button className='btn btn-primary'>Create Group</button>
       </div>
     </>
