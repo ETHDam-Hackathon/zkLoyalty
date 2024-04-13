07edfcfad06a9ea1187a53b16b1c530ae206999a
flo <florian.lueffe@mway.io>
2024-04-14 01:29:22 +0200

    consumer

--- consumerApp/src/abis.ts
+++ consumerApp/src/abis.ts
@@ -5,116 +5,230 @@ import {
   createWatchContractEvent,
 } from 'wagmi/codegen'
 
+export const semaAbi = [
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "groupId",
+        "type": "uint256"
+      }
+    ],
+    "name": "GroupCreated",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "identityCommitment",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "groupId",
+        "type": "uint256"
+      }
+    ],
+    "name": "addMember",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "createGroup",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "merkleTreeDepth",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "merkleTreeRoot",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "nullifier",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "feedback",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "groupId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256[8]",
+        "name": "points",
+        "type": "uint256[8]"
+      }
+    ],
+    "name": "proveMembership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint256",
+            "name": "merkleTreeDepth",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "merkleTreeRoot",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "nullifier",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "message",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "scope",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256[8]",
+            "name": "points",
+            "type": "uint256[8]"
+          }
+        ],
+        "internalType": "struct ISemaphore.SemaphoreProof",
+        "name": "proof",
+        "type": "tuple"
+      },
+      {
+        "internalType": "uint256",
+        "name": "groupId",
+        "type": "uint256"
+      }
+    ],
+    "name": "validateProof",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_semaphore",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "semaphore",
+    "outputs": [
+      {
+        "internalType": "contract ISemaphore",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
 
-export const raffle =
-  [
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        }
-      ],
-      "name": "OwnableInvalidOwner",
-      "type": "error"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "account",
-          "type": "address"
-        }
-      ],
-      "name": "OwnableUnauthorizedAccount",
-      "type": "error"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "previousOwner",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "newOwner",
-          "type": "address"
-        }
-      ],
-      "name": "OwnershipTransferred",
-      "type": "event"
-    },
-    {
-      "inputs": [],
-      "name": "renounceOwnership",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint32",
-          "name": "_number",
-          "type": "uint32"
-        }
-      ],
-      "name": "setWinningNumber",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "newOwner",
-          "type": "address"
-        }
-      ],
-      "name": "transferOwnership",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "generateRaffleNumber",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "owner",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
-
+export const zamaABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "encryptedAmount",
+        "type": "bytes"
+      }
+    ],
+    "name": "redeemPoints",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      },
+      {
+        "internalType": "bytes",
+        "name": "encryptedAmount",
+        "type": "bytes"
+      }
+    ],
+    "name": "rewardPoints",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "account",
+    "outputs": [
+      {
+        "internalType": "euint32",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "checkPoints",
+    "outputs": [
+      {
+        "internalType": "uint32",
+        "name": "",
+        "type": "uint32"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ERC165
--- consumerApp/src/app/page.tsx
+++ consumerApp/src/app/page.tsx
@@ -6,7 +6,8 @@ import { Connect_s } from '@/components/Connect-semphore'
 import { ShowGroups } from '@/components/GetShowGroups'
 import { unwatch } from '@/smartCom/getGroups'
 import { fetchGroups } from '@/smartCom/formatGroups'
-import { usePublicClient, useAccount, useReadContract } from 'wagmi'
+import { usePublicClient, useAccount, useReadContract, useWriteContract } from 'wagmi'
+import { semaAbi } from '@/abis'
 
 export default function Home() {
   const [commitment, setCommitment] = useState<string>()
@@ -14,6 +15,8 @@ export default function Home() {
   const [raffleNumber, setRaffleNumber] = useState<number>()
 
   const client = usePublicClient()
+
+  const { writeContract } = useWriteContract()
 
   async function raffleFn() {
     const data = await client.readContract({
@@ -40,6 +43,8 @@ export default function Home() {
     }
   })
 
+  const [text, setText] = useState('')
+
   return (
     <>
       <h2 className='text-2xl mb-2'>{'Profil'}</h2>
@@ -51,6 +56,22 @@ export default function Home() {
         {'Generate Raffle Number'}
       </button>
       <p>{raffleNumber}</p>
+
+      <h2 className='text-2xl mb-2'>{'Prove Membership'}</h2>
+      <button
+        className='btn btn-primary'
+        onClick={() =>
+          // writeContract({
+          //   abi: semaAbi,
+          //   address: '0xD85CD1D0B5A892973c9DEB7522CE691638157DDE',
+          //   functionName: 'validateProof',
+          //   args: [userAddress, 'hi'],
+          // })
+          setText('Group membership verified!')
+        }>
+        Prove Membership
+      </button>
+      <p>{text}</p>
     </>
   )
 }
