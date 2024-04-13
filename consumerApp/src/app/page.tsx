f6cee8ed16b29ed259908b8f4cf5b6ff477b68ab
Benjamin Taut <benjamin.taut@mway.io>
2024-04-13 17:17:25 +0200

    fixed routing

--- consumerApp/src/app/page.tsx
+++ consumerApp/src/app/page.tsx
@@ -1,16 +1,11 @@
 'use client'
 import { CardList } from '@/components/CardList'
-import { EXAMPLE_ITEMS } from '../examples/examples'
+import { EXAMPLE_ITEMS } from './examples/examples'
 import React from 'react'
 import { useAccount } from 'wagmi'
 import { Connect_s } from '@/components/Connect-semphore'
 
 export default function Home() {
-  const account = useAccount()
-  const test = () => {
-    console.log(account)
-  }
-
   return (
     <>
       <h2 className='text-2xl mb-2'>{'Profil'}</h2>
